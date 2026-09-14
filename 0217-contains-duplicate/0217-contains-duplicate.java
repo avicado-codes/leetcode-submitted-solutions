6e30f08bf906944adbe7d9e3ba2bd1f7f4d12dd3
Avirupa Malakar <avirupamalakar@gmail.com>
2026-09-15 01:17:51 +0530

    Time: 19 ms (48.17%), Space: 111.1 MB (5.49%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(hmap.containsKey(nums[i])){
+                return true;
+            }
+            hmap.put(nums[i],1);
+        }
+        return false;
+    }
+}
